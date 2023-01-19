438053c274ea0bd368ec71b4ce9a6ffe552faccb
IssaAbDev <issaab.dev@gmail.com>
2023-01-19 10:54:59 +0300

    Add projects and Test

---
+++ JenkinsLearn/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,20 @@
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string Expected = "Hello World!";
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                ConsoleApp1.Program.Main(null);
+
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(Expected, result);
+            }
+        }
+    }
+}
