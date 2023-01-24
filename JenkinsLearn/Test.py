2c6b07e89ad722d249a7bce5da0d7a313b8e5eb5
IssaAbDev <issaab.dev@gmail.com>
2023-01-24 11:30:06 +0300

    Add Test.py

---
+++ JenkinsLearn/Test.py
@@ -0,0 +1,24 @@
+import argparse
+
+
+#
+# This module builds the Wayland Build software.
+#
+nobuild = False       # TESTING
+increment_ver = False # increment version
+build_doc = False     # Build documentation
+branch_name = "master"
+
+# Instantiate the parser
+parser = argparse.ArgumentParser(description='Optional app description')
+parser.add_argument('--nobuild', action='store_true', help='No build solution')                   
+parser.add_argument('--increment_ver', action='store_true', help='Increment version')
+parser.add_argument('--build_doc', action='store_true', help='Generate Doxygen')
+parser.add_argument('--branch_name', help='Branch name for installer')
+
+args = parser.parse_args()
+
+nobuild = args.nobuild
+increment_ver = args.increment_ver
+build_doc = args.build_doc
+
