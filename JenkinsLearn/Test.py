7daaffbafbf915bc5e2006cd25d6979d591b5a8f
IssaAbDev <issaab.dev@gmail.com>
2023-01-25 09:57:52 +0300

    1994

--- JenkinsLearn/Test.py
+++ JenkinsLearn/Test.py
@@ -14,14 +14,14 @@ parser = argparse.ArgumentParser(description='Optional app description')
 parser.add_argument('--nobuild', action='store_true', help='No build solution')                   
 parser.add_argument('--increment_ver', action='store_true', help='Increment version')
 parser.add_argument('--build_doc', action='store_true', help='Generate Doxygen')
-parser.add_argument('--branch_name', help='Branch name for installer')
+parser.add_argument('branch_name', help='Branch name for installer')
 
 args = parser.parse_args()
 
 nobuild = args.nobuild
 increment_ver = args.increment_ver
 build_doc = args.build_doc
-
+branch_name = args.branch_name
 
 
 
@@ -34,7 +34,7 @@ os.system('echo eb_build={} increment_ver = {} , build_doc = {}> Hi.txt'.format(
 
 log_file = open("C:\Jenkins\Slave\JenkinsLearn\Hi.txt", "a")
 log_file.truncate()
-log_file.write("eb_build={} increment_ver = {} , build_doc = {}\n".format(WaylandDir,increment_ver,build_doc))
+log_file.write("eb_build={} increment_ver = {} , build_doc = {} , branch_name={}\n".format(WaylandDir,increment_ver,build_doc,branch_name))
 log_file.write("build_eb log file\n")
 log_file.write("#################################################\n")
 log_file.write("\n#################################################\n")
