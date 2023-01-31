4366db325146a0a99b83109f70e0d2e3a6e7201e
IssaAbDev <issaab.dev@gmail.com>
2023-01-31 12:18:29 +0300

    add PyGithub in python

--- JenkinsLearn/GitHubAPI.py
+++ JenkinsLearn/GitHubAPI.py
@@ -1,3 +1,27 @@
-import os
+import requests
+from pprint import pprint
 
-print(f"ss")
+import base64
+from github import Github
+
+
+# github username
+username = "DevIssaAb"
+# url to request
+url = f"https://api.github.com/users/{username}"
+# make the request and return the json
+user_data = requests.get(url).json()
+# pretty print JSON data
+pprint(user_data)
+
+
+
+# Github username
+username = "DevIssaAb"
+# pygithub object
+g = Github()
+# get that user by username
+user = g.get_user(username)
+
+for repo in user.get_repos():
+    print(repo)
