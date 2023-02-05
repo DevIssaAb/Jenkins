f9911f504349cae34aa6a53db3ed49bb5d515af0
DevIssaAb <120650766+DevIssaAb@users.noreply.github.com>
2023-02-05 16:19:43 +0300

    s

--- JenkinsLearn/ConsoleApp1/Program.cs
+++ JenkinsLearn/ConsoleApp1/Program.cs
@@ -5,7 +5,7 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello Issa!");
+            Console.WriteLine("Hello Issa1994");
             //Git Hub 1998
             //new push
             //666
