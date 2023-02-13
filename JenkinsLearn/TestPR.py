9c13e5f720cddf5e3d246854aadc33dc15f7cc72
DevIssaAb <issaab.dev@gmail.com>
2023-02-13 14:27:30 +0300

    Add file python

---
+++ JenkinsLearn/TestPR.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+username = "DevIssaAb"
+repo ='Jenkins'
+token='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'
+
+data = {
+        'owner': username,
+        'repo': repo,
+        "title": "Amazing new feature",
+        "body": "Please pull these awesome changes in!",
+        "head": "issa",
+        "base": "main",
+       
+}
+
+r=requests.post(f'https://api.github.com/repos/{username}/{repo}/pulls',    headers = {
+        "Authorization": "Bearer   {0}".format(token),
+        "Accept": "application/vnd.github+json" ,
+        "X-GitHub-Api-Version": "2022-11-28"
+        },data=json.dumps(data))
+
+
+if not r.ok:
+    print("Request Failed: {0}".format(r.text))
+    print(r.content)
+    #print (r.json())
