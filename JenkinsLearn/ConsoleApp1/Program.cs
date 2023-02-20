7636431a5dbf9f75575d5978aa25e98bc2052ac7
DevIssaAb <120650766+DevIssaAb@users.noreply.github.com>
2023-02-20 15:09:12 +0300

    Update Program.cs

--- JenkinsLearn/ConsoleApp1/Program.cs
+++ JenkinsLearn/ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
         {
           
 #if REVIT2020
-            Console.WriteLine("Hello REVIT2020");
+        
             File.AppendAllLines(@"C:\Users\modar\Desktop\jenkins\Jenkins\JenkinsLearn\ConsoleApp1\bin\Debug\net7.0\test.txt",new string[] { "Hello REVIT2020" });
 #else
             Console.WriteLine("Hello Issa1994");
