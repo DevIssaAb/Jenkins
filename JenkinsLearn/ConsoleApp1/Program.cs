ed8e708c41d6567836c08fec50ec3e952f5e0a3b
DevIssaAb <issaab.dev@gmail.com>
2023-02-20 15:02:16 +0300

    222

--- JenkinsLearn/ConsoleApp1/Program.cs
+++ JenkinsLearn/ConsoleApp1/Program.cs
@@ -1,10 +1,7 @@
 namespace ConsoleApp1
 {
-  
     public class Program
     {
-      
-      
         public static void Main(string[] args)
         {
           
@@ -12,17 +9,11 @@ namespace ConsoleApp1
             Console.WriteLine("Hello REVIT2020");
             File.AppendAllLines(@"C:\Users\modar\Desktop\jenkins\Jenkins\JenkinsLearn\ConsoleApp1\bin\Debug\net7.0\test.txt",new string[] { "Hello REVIT2020" });
 #else
-            //dasd
             Console.WriteLine("Hello Issa1994");
-          //cxzcz
             File.AppendAllLines(@"C:\Users\modar\Desktop\jenkins\Jenkins\JenkinsLearn\ConsoleApp1\bin\Debug\net7.0\test.txt",new string[] { "Hello Issa1994" });
 
 #endif
-//dasds
             Console.ReadKey();
-          
-          //cxzc
-//czxc
         }
     }
 }
