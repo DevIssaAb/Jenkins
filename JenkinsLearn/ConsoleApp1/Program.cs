48fe44d24d9e288f51b8141f5f6c7b5ded40c85e
DevIssaAb <issaab.dev@gmail.com>
2023-02-20 15:09:34 +0300

    64

--- JenkinsLearn/ConsoleApp1/Program.cs
+++ JenkinsLearn/ConsoleApp1/Program.cs
@@ -4,9 +4,9 @@ namespace ConsoleApp1
     {
         public static void Main(string[] args)
         {
-          
+
 #if REVIT2020
-        
+            Console.WriteLine("Hello REVIT2020");
             File.AppendAllLines(@"C:\Users\modar\Desktop\jenkins\Jenkins\JenkinsLearn\ConsoleApp1\bin\Debug\net7.0\test.txt",new string[] { "Hello REVIT2020" });
 #else
             Console.WriteLine("Hello Issa1994");
